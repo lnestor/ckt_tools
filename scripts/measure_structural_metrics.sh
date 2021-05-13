0d77da0cfeaed6f8824afd8637a7e7f909af5be3
Lucas Nestor <lucas15nestor@gmail.com>
2021-05-13 15:15:41 -0400

    Add scripts to mass measure metrics on a directory

---
+++ scripts/measure_structural_metrics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This file measures CNF metrics on all verilog files in a directory
+
+header="--csv_header"
+
+for FILE in $1/*.v; do
+  cat $FILE | sed "s/module .*(/module $(basename $FILE .v)(/" | xargs -0 python ckt_tools/ckt_metrics.py --csv $header $2
+  header=""
+done
