dc8819a112bfe64639045feef6cf76905ac03932
Lucas Nestor <lucas15nestor@gmail.com>
2021-05-24 23:43:38 -0400

    Implement PCA with standardization of independent variables

--- ckt_tools/pca.py
+++ ckt_tools/pca.py
@@ -1,35 +1,48 @@
+import argparse
+from datetime import datetime
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
-metrics = np.genfromtxt("metrics.csv", delimiter=",")
-metrics = metrics[1:]
+# temp
+import matplotlib.pyplot as plt
 
-cnf = np.genfromtxt("cnf.csv", delimiter=",")
-cnf = cnf[1:]
-cnf = [row[1:] for row in cnf]
+def plot_components(x, i1, i2):
+    plt.scatter(x[:,i1], x[:,i2])
+    # plt.xlim(-6, 6)
+    # plt.ylim(-6, 6)
+    plt.show()
 
-# cnf_to_fit = 0
+def plot_dep_vars(x, y, figures_dir):
+    for yi in range(len(y[0])):
+        for xi in range(len(x[0])):
+            plt.scatter(x[:,xi], y[:,yi])
+            plt.xlim(x.min(), x.max())
+            plt.title("X Index: %i, Y Index: %i" % (xi, yi))
 
-# first_row = metrics[0]
-# first_row = np.append(first_row, cnf[0][cnf_to_fit])
-# to_fit = np.array(first_row)
+            date_name = datetime.now().strftime("%m%d%Y_%H%M%S")
+            filename = "x%i_y%i_%s" % (xi, yi, date_name)
+            path = "%s/%s" % (figures_dir, filename)
+            plt.savefig(path, bbox_inches="tight")
+            plt.clf()
 
-# for i in range(1, len(metrics)):
-#     row = metrics[i]
-#     row = np.append(row, cnf[i][cnf_to_fit])
-#     to_fit = np.vstack((to_fit, row))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run PCA on a set of independent variables")
+    parser.add_argument("ind_vars_file", metavar="independent_variables", help="A CSV containing the independent variables")
+    parser.add_argument("--dep_vars_file", metavar="dependent_variables", help="A CSV containing the dependent variables")
+    parser.add_argument("--figures_dir", help="A directory to save generated figures in")
 
-# pca = PCA(n_components=3)
-# pca.fit(to_fit)
-# fitted = pca.transform(to_fit)
+    args = parser.parse_args()
 
-pca = PCA(n_components=5)
-pca.fit(metrics)
-fitted = pca.transform(metrics)
+    x = np.genfromtxt(args.ind_vars_file, delimiter=",")
+    x = x[1:]
+    x = StandardScaler().fit_transform(x)
 
-components = pca.components_[0]
-l1 = [float(c) for c in components]
+    pca = PCA()
+    projected = pca.fit_transform(x)
 
-# np.savetxt("fitted.csv", fitted, delimiter=",")
+    if args.dep_vars_file:
+        y = np.genfromtxt(args.dep_vars_file, delimiter=",")
+        y = y[1:]
 
-import pdb; pdb.set_trace()
+        plot_dep_vars(projected, y, args.figures_dir)
