d244878be4d87af0d6258fdf12f61d086624220c
Lucas Nestor <lucas15nestor@gmail.com>
2021-06-02 10:37:43 -0400

    Add manual PC calculation

--- ckt_tools/pca.py
+++ ckt_tools/pca.py
@@ -3,8 +3,6 @@ from datetime import datetime
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-
-# temp
 import matplotlib.pyplot as plt
 
 def plot_components(x, i1, i2):
@@ -12,8 +10,6 @@ def plot_components(x, i1, i2):
     plt.title("Index 1: %i, Index 2: %i" % (i1, i2))
     plt.xlim(x.min(), x.max())
     plt.ylim(x.min(), x.max())
-    # plt.xlim(-6, 6)
-    # plt.ylim(-6, 6)
     plt.show()
 
 def plot_dep_vars(x, y, figures_dir):
@@ -29,20 +25,39 @@ def plot_dep_vars(x, y, figures_dir):
             plt.savefig(path, bbox_inches="tight")
             plt.clf()
 
+def calc_pc(x, pc_weights):
+    pc_vals = [sum([a * b for a, b in zip(sample, pc_weights)]) for sample in x]
+
+    for pc_val in pc_vals:
+        print(pc_val)
+
+    return pc_vals
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run PCA on a set of independent variables")
     parser.add_argument("ind_vars_file", metavar="independent_variables", help="A CSV containing the independent variables")
     parser.add_argument("--dep_vars_file", metavar="dependent_variables", help="A CSV containing the dependent variables")
     parser.add_argument("--figures_dir", help="A directory to save generated figures in")
+    parser.add_argument("--calc", help="A CSV file to calculate a PC on. Note that these samples are not included in the PCA.")
+    parser.add_argument("--pc", type=int, help="The principle component to calculate when the '--calc' option is used.")
 
     args = parser.parse_args()
 
+    scaler = StandardScaler()
     x = np.genfromtxt(args.ind_vars_file, delimiter=",")
     x = x[1:]
-    x = StandardScaler().fit_transform(x)
+    x = scaler.fit_transform(x)
 
     pca = PCA()
     projected = pca.fit_transform(x)
+
+    if args.calc:
+        calc_x = np.genfromtxt(args.calc, delimiter=",")
+        calc_x = calc_x[1:]
+        calc_x = scaler.transform(calc_x)
+
+        pc_index = 0 if not args.pc else args.pc - 1
+        pc_vals = calc_pc(calc_x, pca.components_[pc_index])
 
     if args.dep_vars_file:
         y = np.genfromtxt(args.dep_vars_file, delimiter=",")
